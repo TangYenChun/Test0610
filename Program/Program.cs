a9df2b7287a100fde604820f9f73eee9f1828c51
Bella <bella4232@gmail.com>
2022-06-10 10:42:02 +0800

    加入專案檔案。

---
+++ Program/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private void Question1(int[] nums, int target)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = 0; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        Console.WriteLine($"[{nums[i]}, {nums[j]}]");
+                    }
+                }
+            }
+        }
+
+        private void Question5(int n)
+        {
+
+        }
+    }
+}
